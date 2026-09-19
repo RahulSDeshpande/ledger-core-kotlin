4e1e0867157ec1837c7c0107fc10cbccfcb0d9f6
Rahul Deshpande <rahul.deshpande92@gmail.com>
2026-09-20 01:30:57 +0400

    chore: gradle skeleton, kotlin jvm, junit5

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    kotlin("jvm") version "2.2.20"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("com.rahuld.ledgercore.MainKt")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging { events("passed", "failed") }
+}
+
